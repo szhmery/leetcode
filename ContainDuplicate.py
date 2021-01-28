a12d4e33ec845af90e11e19c9530d24811600c4d
Zhaohui Sun <szhmery@gmail.com>
2021-01-28 16:33:24 +0800

    commit ContainDuplicate and ReverseString code

---
+++ ContainDuplicate.py
@@ -0,0 +1,25 @@
+from typing import List
+
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        nums.sort()
+        for i in range(len(nums)-1):
+            if nums[i] == nums[i+1]:
+                return True
+
+        return False
+
+        # for i in range(len(nums)):
+        #     if nums.count(nums[i]) > 1:
+        #         return True
+        # return False
+if __name__ == "__main__":
+    solution = Solution()
+    nums = [1, 2, 3, 4, 5]
+    is_duplicate = solution.containsDuplicate(nums)
+    print("Is duplicated: {}".format(is_duplicate))
+
+    nums = [1,2,3,1]
+    is_duplicate = solution.containsDuplicate(nums)
+    print("Is duplicated: {}".format(is_duplicate))
---
+++ ReverseString.py
@@ -0,0 +1,27 @@
+from typing import List
+
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        half_lenght = int(len(s)/2)
+        for k in range(half_lenght):
+            # temp = s[k]
+            # s[k] = s[len(s)-1-k]
+            # s[len(s)-1-k] = temp
+            s[k], s[len(s) - 1 - k] = s[len(s) - 1 - k], s[k]
+
+    def reverseString_answer(self, s: List[str]) -> None:
+        s.reverse()
+
+
+if __name__=="__main__":
+    solution = Solution()
+    string1=["h","e","l","l","o"]
+    print("before reverse:{}".format(string1))
+    solution.reverseString(string1)
+    print("after reverse:{}".format(string1))
+
+    solution.reverseString_answer(string1)
+    print("after reverse:{}".format(string1))
