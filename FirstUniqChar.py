85814e36d0715a89fb7fdb7ff064ddea58c01c1b
Zhaohui Sun <szhmery@gmail.com>
2021-02-20 14:59:53 +0800

    commit ReverseInteger and FirstUniqChar

---
+++ FirstUniqChar.py
@@ -0,0 +1,36 @@
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        if len(s) == 1:
+            return 0
+
+        for i in range(len(s)):
+            if s.count(s[i]) > 1:
+                continue
+            else:
+                return i
+        return -1
+
+if __name__=='__main__':
+    solution = Solution()
+    s = "leetcode"
+    index = solution.firstUniqChar(s)
+    print("First unique character index: {}".format(index))
+
+    s = "loveleetcode"
+    index = solution.firstUniqChar(s)
+    print("First unique character index: {}".format(index))
+
+    s = "cc"
+    index = solution.firstUniqChar(s)
+    print("First unique character index: {}".format(index))
+
+    s = "z"
+    index = solution.firstUniqChar(s)
+    print("First unique character index: {}".format(index))
+
+    s = "aadadaad"
+    index = solution.firstUniqChar(s)
+    print("First unique character index: {}".format(index))
+
+
+
---
+++ ReverseInteger.py
@@ -0,0 +1,42 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        x = str(x)
+        y=''
+        if x[0] == '-':
+            y='-'
+            for i in range(1,len(x)):
+                y=y+x[len(x)-i]
+
+        else:
+            for i in range(len(x)):
+                y=y+x[len(x)-1-i]
+
+        y = int(y)
+        if y >= 2**31-1 or y <= -2**31: return 0
+        return y
+if __name__ == '__main__':
+    solution = Solution()
+    x = 123
+    result = solution.reverse(x)
+    print("before reverse:{}".format(x))
+    print("after reverse:{}".format(result))
+
+    x = -123
+    result = solution.reverse(x)
+    print("before reverse:{}".format(x))
+    print("after reverse:{}".format(result))
+
+    x = 120
+    result = solution.reverse(x)
+    print("before reverse:{}".format(x))
+    print("after reverse:{}".format(result))
+
+    x = 0
+    result = solution.reverse(x)
+    print("before reverse:{}".format(x))
+    print("after reverse:{}".format(result))
+
+    x=1534236469
+    result = solution.reverse(x)
+    print("before reverse:{}".format(x))
+    print("after reverse:{}".format(result))
